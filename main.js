513a7b2f5724c55882e191f24219e315bc49815b
Tom Han <tomhyhan@gmail.com>
2021-01-21 04:27:28 -0800

    Added better responsive design for Galaxy

--- main.js
+++ main.js
@@ -17,6 +17,8 @@ document.addEventListener('scroll', () => {
 const navbarMenu = document.querySelector('.navbar__menu');
 
 navbarMenu.addEventListener('click', (e) => {
+  menus.classList.remove('nabar__menu--visible');
+  navbar.classList.remove('navbar-bg');
   scrollInto(e);
 });
 
@@ -104,4 +106,14 @@ const blogUrl = document.querySelector('.blog-url');
 
 blogUrl.addEventListener('click', () => {
   window.open('https://project-myblog.herokuapp.com/');
+});
+
+// =======Navbar toggle =================
+
+const bars = document.querySelector('.navbar__bars');
+const menus = document.querySelector('.navbar__menu');
+
+bars.addEventListener('click', () => {
+  menus.classList.toggle('nabar__menu--visible');
+  navbar.classList.toggle('navbar-bg');
 });
